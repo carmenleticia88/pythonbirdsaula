4f20f7190a49b3a0541765caf3cff503d828a94b
Carmen-PC <carmenleticia88@hotmail.com>
2022-05-02 22:57:23 -0300

    # Finalização jogo Python Birds

--- fase.py
+++ fase.py
@@ -106,6 +106,12 @@ class Fase():
         :param tempo: tempo para o qual devem ser calculados os pontos
         :return: objeto do tipo Ponto
         """
+        for passaro in self._passaros:
+            passaro.calcular_posicao(tempo)
+            for alvo in self._obstaculos + self._porcos:
+                passaro.colidir(alvo, self.intervalo_de_colisao)
+            passaro.colidir_com_chao()
+
         pontos=[self._transformar_em_ponto(a) for a in self._passaros+self._obstaculos+self._porcos]
 
         return pontos
@@ -117,11 +123,11 @@ class Fase():
         for porco in self._porcos:
             if porco.status == ATIVO:
                 return True
-            return False
+        return False
 
     def _possui_passaros_ativos(self):
         for passaro in self._passaros:
             if passaro.status == ATIVO:
                 return True
-            return False
+        return False
 
